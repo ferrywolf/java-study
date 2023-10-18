c596e53d521c13d7ede56465b8dd3e0c575fcb11
黄远 <>
2023-10-18 21:58:05 +0800

    测试项目

---
+++ mq/src/com/mdx/TestController.java
@@ -0,0 +1,15 @@
+package com.mdx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author huangy
+ * @since 2023/10/15 13:21
+ */
+@SpringBootApplication
+public class TestController {
+    public static void main(String[] args) {
+        SpringApplication.run(TestController.class,args);
+    }
+}
