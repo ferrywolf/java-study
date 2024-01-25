12ccf547bc71efc7ca461a5cb524f165b6e25af8
ferrywolf <1511370671@qq.com>
2024-01-25 23:25:38 +0800

    更换java版本

--- mq/src/com/mdx/MqApplication.java
+++ mq/src/com/mdx/MqApplication.java
@@ -3,13 +3,11 @@ package com.mdx;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * @author huangy
- * @since 2023/10/15 13:21
- */
 @SpringBootApplication
-public class TestController {
+public class MqApplication {
+
     public static void main(String[] args) {
-        SpringApplication.run(TestController.class,args);
+        SpringApplication.run(MqApplication.class, args);
     }
+
 }
